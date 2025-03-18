5af928cc3f8e5c9532770184a4ed64fb99fdecb7
Levi-Wani-Scopas <leviscopas17@gmail.com>
2025-03-18 22:39:05 +0300

    Patient Login Activity

--- app/src/main/java/com/example/smams/SplashActivity.java
+++ app/src/main/java/com/example/smams/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.example.smams;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler = new Handler();
+    ImageView splash1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        splash1= findViewById(R.id.splashPic);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, AuthenticationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
+
