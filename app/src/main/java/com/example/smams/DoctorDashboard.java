3f9ee7b94006e2f507a412ff044f2bfe4f0a0e26
Levi-Wani-Scopas <leviscopas17@gmail.com>
2025-03-19 17:11:50 +0300

    Working on Patient login

---
+++ app/src/main/java/com/example/smams/DoctorDashboard.java
@@ -0,0 +1,4 @@
+package com.example.smams;
+
+public class DoctorDashboard {
+}
--- app/src/main/java/com/example/smams/PatientLoginActivity.java
+++ app/src/main/java/com/example/smams/PatientLoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.smams;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+
 public class PatientLoginActivity extends AppCompatActivity {
+
+
+    private String role;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_patient_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
